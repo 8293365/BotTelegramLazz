0d0af89fb24e8c73ba4f1e8cf115125c20cf3834
LAZZARETTO RUBEN <8293365@itisrossi.edu.it>
2024-12-06 11:52:42 +0100

    stuff

---
+++ AvpBotTelegram/src/main/java/org/example/TelBot.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+public class TelBot {
+}
---
+++ AvpBotTelegram/src/main/java/org/example/WebScraper.java
@@ -0,0 +1,4 @@
+package org.example;
+//selenium???? when a page has elements of javascript which cannot be accessed by simply reading the html file
+public class WebScraper {
+}
