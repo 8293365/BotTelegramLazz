36c337b4c0c0fffa5b1e3123362c4c554c6fa07d
Ruben Lazzaretto <8293365@itisrossi.vi.it>
2024-12-11 18:41:54 +0100

    telbot_base_setup

--- AvpBotTelegram/src/main/java/org/example/Main.java
+++ AvpBotTelegram/src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import slf4j
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            String botToken = "8084141518:AAG1KfsjjV2weofReaBnniG-RUiH-p40EII";
+            TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+            botsApplication.registerBot(botToken, new TelBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
--- AvpBotTelegram/src/main/java/org/example/TelBot.java
+++ AvpBotTelegram/src/main/java/org/example/TelBot.java
@@ -1,11 +1,21 @@
 package org.example;
 
-import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+//import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
+//SLF4J stands for Simple Logging Facade for Java
+public class TelBot implements LongPollingSingleThreadUpdateConsumer {
 
-public class TelBot {
+    @Override
+    public void consume(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            System.out.println(update.getMessage().getText());
+        }
+    }
+
+
 }
